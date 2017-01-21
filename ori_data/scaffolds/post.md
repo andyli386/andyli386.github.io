635b28e8c29fe180523b0a084642f84873026d20
Vincent <andyli386@gmail.com>
2017-01-21 15:56:56 +0800

    Site updated: 2017-01-21 15:56:56

--- ori_data/scaffolds/post.md
+++ ori_data/scaffolds/post.md
@@ -6,6 +6,8 @@ tags:
 
 
 ---
-博客地址：52ml.me
+博客地址：[52ml.me](http://www.52ml.me)<br>
 原创文章，版权声明：自由转载-非商用-非衍生-保持署名 | [Creative Commons BY-NC-ND 3.0](http://creativecommons.org/licenses/by-nc-nd/3.0/deed.zh)
+<br>
 ---
+
--- ori_data/sh/publish.sh
+++ ori_data/sh/publish.sh
@@ -3,6 +3,7 @@ cd $BLOGHOME
 cp themes/yilia/_config.yml source/ori_data/themes/yilia/config.yml
 cp _config.yml source/ori_data/config.yml
 cp source/_posts/* source/ori_data/posts/
+cp source/assets/* source/ori_data/assets/
 cp scaffolds/* source/ori_data/scaffolds/
 cd $BLOGHOME/source/
 git add .
