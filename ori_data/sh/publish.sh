d6f6573891e7b3d3956e4c3a4a1da321363b2a00
Vincent <andyli386@gmail.com>
2017-02-05 15:19:39 +0800

    Site updated: 2017-02-05 15:19:38

--- ori_data/sh/publish.sh
+++ ori_data/sh/publish.sh
@@ -2,9 +2,9 @@
 cd $BLOGHOME
 cp themes/yilia/_config.yml source/ori_data/themes/yilia/config.yml
 cp _config.yml source/ori_data/config.yml
-cp source/_posts/* -r source/ori_data/posts/
-cp source/assets/* -r source/ori_data/assets/
-cp scaffolds/* source/ori_data/scaffolds/
+cp -r source/_posts/*  source/ori_data/posts/
+cp -r source/assets/*  source/ori_data/assets/
+cp -r scaffolds/* source/ori_data/scaffolds/
 cd $BLOGHOME/source/
 git add .
 git commit -m "Update blog"
